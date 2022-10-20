656c97fd016f650269399555453ba69cc069ae44
AleksiDu <aleksi.duluzauri@gmail.com>
2022-10-20 12:45:07 +0400

    add loading spinner & error handling

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,32 @@
-export default {};
+export default {
+  login() {},
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDFfwbCIyQPHVi1xOxzI8FajbY8FCUFCWo',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log('responseData', responseData);
+      const error = new Error(
+        responseData.error.message.replace(/_+/g, ' ').toLowerCase() ||
+          'Failed to authenticate. Check your login data.'
+      );
+      throw error;
+    }
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localID,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
