d739a54b8daa0f49d1a17de76b030d7a0f80539f
AleksiDu <aleksi.duluzauri@gmail.com>
2022-10-20 11:21:56 +0400

    add authentication page

---
+++ src/store/modules/auth/index.js
@@ -0,0 +1,14 @@
+import actions from './getters.js';
+import getters from './getters.js';
+import mutations from './mutations.js';
+
+export default {
+  state() {
+    return {
+      userId: 'c3',
+    };
+  },
+  mutations,
+  actions,
+  getters,
+};
